9ff95563714b30814ca241ecbb2511f046f41431
deghislain <deghislain@gmail.com>
2024-10-11 13:51:34 -0300

    code clean up

--- test.py
+++
@@ -1,9 +0,0 @@
-def calculate_average(numbers):
-    total = 0
-    for number in numbers:
-        total += number
-    return total / len(numbers)
-
-
-numbers = [10, 20, 30, 40, 50]
-print("Average:", calculate_average(numbers))
